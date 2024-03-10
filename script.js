5ac0745568d58812510fb9a87f1afe30053f2628
NewManuel <100541053+NewManuel@users.noreply.github.com>
2024-03-10 15:27:32 -0500

    Editing Variables

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var allInOne = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*-_+="
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var capitolCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var specialsymbols = ["!","@","#","$","%","^","&","*","-","_","+","="];
+var digits = [1,2,3,4,5,6,7,8,9,0];
 // console.log(lowerCaseList)
 
 
 
 // Write password to the #password input
-function writePassword() {
+function formPassword() {
  let password = "";
   
- const length =9;
+ const length ="";
 
 for (let i = 0; i < length; i++) {
  var num  = Math.floor(Math.random()*allInOne.length);
@@ -19,10 +22,38 @@ for (let i = 0; i < length; i++) {
 return password;
 }
 
+function createPrompt() {
+  var upper = prompt("Wouold you like to use uppercase characters? y/n");
+  var lower = prompt("Do you want lowercase characters? y/n");
+  var num = prompt("Do you want numbers? y/n");
+  var special = prompt("Do you want special characters? y/n");
+
+  if (upper === "y") {
+    selectedList = uppercaseList.concat(selectedList);
+  } else {
+    console.log("Wrong input!");
+  }
+  if (lower === "y") {
+    selectedList = alphList.concat(selectedList);
+  } else {
+    console.log("Wrong input!");
+  }
+  if (num === "y") {
+    selectedList = numList.concat(selectedList);
+  } else {
+    console.log("Wrong input!");
+  }
+  if (special === "y") {
+    selectedList = specialList.concat(selectedList);
+  } else {
+    console.log("Wrong input!");
+  }
+}
+
 // Add event listener to generate button
 document.getElementById("generate").addEventListener("click",  function() {
-  document.getElementById("password").value = writePassword();
+  document.getElementById("password").value = formPassword();
 //.value instead of .innertext
   
-  console.log(document.getElementById("password").innertext = writePassword())
+  console.log(document.getElementById("password").innertext = formPassword())
 })
